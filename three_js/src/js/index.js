1dbf5a5f1682e37a4a6bf44c7c4daefdffe7cc24
khwoo91 <168423069+khwoo91@users.noreply.github.com>
2025-03-10 11:31:51 +0900

    11

--- three_js/src/js/index.js
+++ three_js/src/js/index.js
@@ -1,17 +1,22 @@
 import * as THREE from "three";
 
+document.body.style.margin = '0';
+document.body.style.overflow = 'hidden';
+
 console.log(THREE);
 
 
 // 장면 생성
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xFFE187)
+scene.background = null;
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(3, 3, 3);
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+    alpha: true
+})
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -30,3 +35,12 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 renderer.render(scene, camera);
+
+// 창 크기 조절
+const onWindowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
+window.addEventListener('resize', onWindowResize);
